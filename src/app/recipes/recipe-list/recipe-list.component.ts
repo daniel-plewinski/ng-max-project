ce2618a75b91f712655e46a750bea80a2a79e8cb
daniel-plewinski <none>
2018-07-11 20:27:01 +0200

    Recipes view completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is just a test',
+      'https://www.maxpixel.net/static/photo/1x/Cook-Cooking-Recipe-Culinary-Food-Restaurant-867989.jpg'),
+    new Recipe('Test recipe', 'This is just a test',
+      'https://www.maxpixel.net/static/photo/1x/Cook-Cooking-Recipe-Culinary-Food-Restaurant-867989.jpg')
+  ];
 
   constructor() { }
 
